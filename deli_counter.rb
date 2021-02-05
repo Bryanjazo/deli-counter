3291dead4d504e86fd24312e76b5111cdac49618
Bryanjazo <bryanjazo88@gmail.com>
2021-02-04 17:45:28 -0800

    commit tree 23b48e77c2e0a05962b1b2a3fcdebd61fb6d8bb8

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+require 'pry'
+def line(katz_deli)
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    else 
+         deli = "The line is currently:" 
+         katz_deli.each_with_index do |name, index|
+            deli << (" #{index + 1}. #{name}") 
+        end
+        puts deli 
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli.push(name)
+    puts"Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(number)
+    if number.length === 0
+        puts "There is nobody waiting to be served!"
+    else 
+        #binding.pry
+        puts "Currently serving #{number.first}."
+        number.shift
+    end
+end
